cad4eab5bf4a7bee65a20392d386095b5eed4d06
jamshaidazeemofficial <jamshaid@idevnerds.com>
2024-02-13 18:00:37 +0500

    predeploy.sh updated

--- predeploy.sh
+++ predeploy.sh
@@ -1,4 +1,7 @@
 # predeploy.sh
 
-# remove the version hash from our base javascript file for a stable URL
+# remove the version hash from our base javascript file for a stable URL using copy
 find build/static/js -name "main.*.js" -exec cp '{}' build/static/js/main.js \;
+
+# remove the version hash from our base javascript file for a stable URL using mov
+# find build/static/js -name "main.*.js" -exec mv '{}' build/static/js/main.js \;
